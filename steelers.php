814c80c1672a5eaa06a89838101086fa85c7cb54
yas42 <yas42@pitt.edu>
2017-01-12 17:12:38 -0500

    one more new commit

---
+++ steelers.php
@@ -0,0 +1,68 @@
+<?php
+define('CONSUMER_KEY', '8wU6Z7SvF8BRBL0LcPdxfSH75'); 
+define('CONSUMER_SECRET', 'Zwu0cG9IopVBDEgisExhsSZz9LEKyzpLhfQVKxwYWp7YhwvFl6');
+
+$x = base64_encode(rawurlencode(CONSUMER_KEY). ":".rawurlencode(CONSUMER_SECRET));
+$url = "https://api.twitter.com/oauth2/token"; 
+$header = array(); 
+$header[] = "Authorization: Basic $x"; 
+$header[] = "Content-Type: application/x-www-form-urlencoded;charset=UTF-8"; 
+$curl = curl_init(); 
+curl_setopt($curl, CURLOPT_URL, $url); 
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $header); 
+curl_setopt($curl,CURLOPT_POST, 1); 
+curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0); 
+curl_setopt($curl,CURLOPT_POSTFIELDS, "grant_type=client_credentials");
+$output = curl_exec($curl); 
+$result = json_decode($output,true);
+$access_token = $result['access_token'];
+curl_close($curl);
+
+$query = urlencode("steelers"); //include your query
+$search_url = "https://api.twitter.com/1.1/search/tweets.json?q=$query&result_type=recent&count=5"; //retrieve 5 most recent tweets
+$output = @file_get_contents($search_url,false,stream_context_create(array(
+	'http'=>array(
+		'method'=>"GET",
+		'header'=>"Authorization:Bearer $access_token"))));
+$result = json_decode($output,true); //results returned as JSON
+$result = $result['statuses'];
+for ($i=0;$i<count($result);$i++) 
+{
+    $id = $result[$i]['id_str']; //tweet id
+    $username = $result[$i]['user']['name']; //user posting tweet
+    $text = $result[$i]['text']; //tweet text
+    //$created_at = $result[$i]['created_at'] //tweet post time
+}
+
+//echo $result
+$str = my_implode('-',$result);
+echo $str;
+//$senurl = "https://community-sentiment.p.mashape.com/text/";
+//$sendata = array("txt"=>"Steelers");
+//$header = array();
+//$header[] = 'X-Mashape-Key: tkauOXwedGmshdXr1rmc3OE09yl0p1R4SzbjsnD2smREz6oyV5';
+//$header[] ='Content-Type: application/x-www-form-urlencoded';
+//$header[] = 'Accept: application/json';
+//$fields_string = '';
+//foreach($sendata as $key=>$value) { 
+//		$fields_string .= $key.'='.$value.'&'; 
+//}
+
+//rtrim($fields_string, '&');
+//$curl = curl_init();
+//curl_setopt($curl, CURLOPT_URL, $senurl);
+//curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+//curl_setopt($curl,CURLOPT_POST, count($sendata));
+//curl_setopt($curl,CURLOPT_POSTFIELDS, $fields_string);
+//$sentimentdata = curl_exec($curl);
+//curl_close($curl);
+//$sentimentdata= json_decode($sentimentdata,true);
+//$confidence = $sentimentdata['result']['confidence'];
+//$sentiment = $sentimentdata['result']['sentiment'];
+
+
+
+?>
